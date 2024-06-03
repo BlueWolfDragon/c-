46a9e900bf31e359f71d3171e8ed843e30540de0
Brandon Lorenzo <99996583+BlueWolfDragon@users.noreply.github.com>
2024-06-04 01:06:26 +0200

    Update Hola Mundo.cpp

--- Hola Mundo.cpp
+++ Hola Mundo.cpp
@@ -1,8 +1,10 @@
+//importamos librerias
 #include<iostream>
 using namespace std;
 
+//declaramos la funcion principal
 int main(){
-
+//imprimimos en pantalla "Hola Mundo"
 cout<<"Hola Mundo";
 
 }
